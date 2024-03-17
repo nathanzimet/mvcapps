68885fff709ff79d034653556a9ddfc32df6e7a0
priyankagoel <gunjan.priyanka29@gmail.com>
2024-03-17 02:30:11 -0700

    Corrected errors in GridView.

--- src/CALab/GridView.java
+++ src/CALab/GridView.java
@@ -14,7 +14,7 @@ public class GridView  extends View {
         super(model);
 
         //Nathan: just for simplicity
-        int d = ((Grid)model).dim;
+        int d = ((Grid)model).getDim();
 
         this.setLayout(new GridLayout(d, d));
         cellViews = new CellView[d][d];
@@ -26,15 +26,15 @@ public class GridView  extends View {
 
                 //set cell.row and cell.col here;
                 //Nathan: this is what the professor asked for, but it doesn't seem right...
-                ((Grid)model).getCell(i, j).row = i;
-                ((Grid)model).getCell(i, j).col = j;
+                //((Grid)model).getCell(i, j).row = i;
+                //((Grid)model).getCell(i, j).col = j;
             }
         }
     }
 
     public void update() {
         //Nathan: just for simplicity
-        int d = ((Grid)model).dim;
+        int d = ((Grid)model).getDim();
 
         // call update method of each CellView
         for (int i = 0; i < d; i++) {
